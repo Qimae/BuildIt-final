70facec849100ae166077b7d8a43e4a5d17c197f
NathyG2524 <nathygirma2524@gmail.com>
2022-09-05 16:53:04 +0300

    feat: adjust new api

--- backend/builder/urls.py
+++ backend/builder/urls.py
@@ -1,22 +1,25 @@
-from unicodedata import name
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
 from . import views
 
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-router.register(r'pages', views.PagesView, 'todo')
-
 urlpatterns = [
-    path('api/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('', views.index, name='home'),
-    path('add', views.addPage, name='addpage'),
-    path('edit/<id>', views.editPage, name='editpage'),
-    path('page/create', views.savePage, name='create_page'),
-    path('editPage/<id>', views.editPageContent, name='editPageContent'),
-    path('delete/<id>', views.deletePage, name='deletePage'),
-    path('preview/<id>', views.previewPage, name='previewPage')
+	path('', views.apiOverview, name="api-overview"),
+	path('page-list/', views.pageList, name="page-list"),
+	path('page-detail/<str:pk>/', views.pageDetail, name="page-detail"),
+	path('page-create/', views.pageCreate, name="page-create"),
+
+	path('page-update/<str:pk>/', views.pageUpdate, name="page-update"),
+	path('page-delete/<str:pk>/', views.pageDelete, name="page-delete"),
+	path('template-list/', views.templateList, name="template-list"),
+	path('templete-detail/<str:pk>/', views.templeteDetail, name="templete-detail"),
+	path('templete-create/', views.templeteCreate, name="templete-create"),
+
+	path('templete-update/<str:pk>/', views.templateUpdate, name="templete-update"),
+	path('templete-delete/<str:pk>/', views.templeteDelete, name="templete-delete"),
+
+	path('catagory-list/', views.catagoriesList, name="template-list"),
+	path('catagory-detail/<str:pk>/', views.catagoriesDetail, name="catagory-detail"),
+	path('catagory-create/', views.catagoriesCreate, name="catagory-create"),
+
+	path('catagory-update/<str:pk>/', views.catagoriesUpdate, name="catagory-update"),
+	path('catagory-delete/<str:pk>/', views.catagoriesDelete, name="catagory-delete"),
 ]
--- backend/builder/views.py
+++ backend/builder/views.py
@@ -1,82 +1,157 @@
-from http.client import HTTPResponse
 from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse
-from .models import Pages
-from django.contrib.auth.models import User, Group
+from django.http import JsonResponse
+from rest_framework import status
+
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from django.core.serializers import serialize
-from .serializers import PagesSerializer, UserSerializer, GroupSerializer
-from rest_framework import viewsets
-from rest_framework import permissions
-import json
+from .serializers import PagesSerializer, TempletesSerializer, CatagoriesSerializer
+
+from builder.models import Pages, Templetes, Catagories
+
+
+# 1. Import the csrf_exempt decorator
+from django.views.decorators.csrf import csrf_exempt
+
+# 2. Exempt the view from CSRF checks
 
 # Create your views here.
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+
+@api_view(['GET'])
+def apiOverview(request):
+	api_urls = {
+		'List':'/pages-list/',
+		'Detail View':'/pages-detail/<str:pk>/',
+		'Create':'/pages-create/',
+		'Update':'/pages-update/<str:pk>/',
+		'Delete':'/pages-delete/<str:pk>/',
+		}
+	return Response(api_urls)
+
+@api_view(['GET'])
+def pageList(request):
+	pages = Pages.objects.all().order_by('-id')
+	serializer = PagesSerializer(pages, many=True)
+	return Response(serializer.data)
+
+@api_view(['GET'])
+def pageDetail(request, pk):
+	pages = Pages.objects.get(id=pk)
+	serializer = PagesSerializer(pages, many=False)
+	return Response(serializer.data)
+
+@csrf_exempt
+@api_view(['POST'])
+def pageCreate(request):
+	# serializer = PagesSerializer(data=request.data)
+
+	
+    if request.method == 'POST':
+        serializer = PagesSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def pageUpdate(request, pk):
+	page = Pages.objects.get(id=pk)
+	serializer = PagesSerializer(instance=page, data=request.data)
+
+	if serializer.is_valid():
+		serializer.save()
+
+	return Response(serializer.data)
 
 
+@api_view(['DELETE'])
+def pageDelete(request, pk):
+	page = Pages.objects.get(id=pk)
+	page.delete()
 
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
+	return Response('Item succsesfully delete!')
 
-class PagesView(viewsets.ModelViewSet):  
-    serializer_class = PagesSerializer   
-    queryset = Pages.objects.all() 
+#below is where Catagories api is rendered 
 
-def index(request):
-    pages = Pages.objects.all()
-    return render(request, 'pages.html', {"pages":pages})
+@api_view(['GET'])
+def catagoriesList(request):
+	catagories = Catagories.objects.all().order_by('-id')
+	serializer = CatagoriesSerializer(catagories, many=True)
+	return Response(serializer.data)
 
-def addPage(request):
-    return render(request, 'index.html')
-
-def savePage(request):
-    if(request.method=='POST'):
-        html = request.POST['html']
-        css = request.POST['css']
-        page = Pages.objects.create(name="untitled", html=html, css=css)
-        page.save()
-    return JsonResponse({
-        "result": (json.loads.serialize('json', [page]))[0]
-    })
-
-def editPage (request, id):
-    page = Pages.objects.get(pk=id)
-    return render(request, 'index.html', {"page": page})
-
-def editPageContent (request, id):
-    if(request.method=='POST'):
-        html = request.POST['html']
-        css = request.POST['css']
-        page = Pages.objects.get(pk=id)
-        page.html = html
-        page.css = css
-        page.save()
-    return JsonResponse({
-        "result": (json.loads.serialize('json', [page]))[0]
-    })
+@api_view(['GET'])
+def catagoriesDetail(request, pk):
+	catagories = Catagories.objects.get(id=pk)
+	serializer = CatagoriesSerializer(catagories, many=True)
+	return Response(serializer.data)
 
 
-def deletePage(request, id):
-    page = Pages.objects.get(pk=id)
-    if page is None:
-        print("page not found")
-    page.delete()
+@api_view(['POST'])
+def catagoriesCreate(request):
+	serializer = CatagoriesSerializer(data=request.data)
 
-    return JsonResponse({
-        "success": True,
-        "deleted": id
-    })
-def previewPage (request, id):
-    page = Pages.objects.get(pk=id)
-    return render(request, 'preview.html', {"page": page})
+	if serializer.is_valid():
+		serializer.save()
 
+	return Response(serializer.data)
+
+@api_view(['POST'])
+def catagoriesUpdate(request, pk):
+	catagories = Catagories.objects.get(id=pk)
+	serializer = CatagoriesSerializer(instance=page, data=request.data)
+
+	if serializer.is_valid():
+		serializer.save()
+
+	return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def catagoriesDelete(request, pk):
+	catagory = Catagories.objects.get(id=pk)
+	catagory.delete()
+
+	return Response('Item succsesfully delete!')
+
+#below is where Templates api is rendered 
+
+@api_view(['GET'])
+def templateList(request):
+	templetes = Templetes.objects.all().order_by('-id')
+	serializer = TempletesSerializer(templetes, many=True)
+	return Response(serializer.data)
+
+@api_view(['GET'])
+def templeteDetail(request, pk):
+	templetes = Templetes.objects.get(id=pk)
+	serializer = TempletesSerializer(templetes, many=True)
+	return Response(serializer.data)
+
+
+@api_view(['POST'])
+def templeteCreate(request):
+	serializer = TempletesSerializer(data=request.data)
+
+	if serializer.is_valid():
+		serializer.save()
+
+	return Response(serializer.data)
+
+@api_view(['POST'])
+def templateUpdate(request, pk):
+	page = Pages.objects.get(id=pk)
+	serializer = TempletesSerializer(instance=page, data=request.data)
+
+	if serializer.is_valid():
+		serializer.save()
+
+	return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def templeteDelete(request, pk):
+	page = Templetes.objects.get(id=pk)
+	page.delete()
+
+	return Response('Item succsesfully delete!')
