64cf67fdad8879851c42cf28fc46e3313d866be7
NathyG2524 <nathygirma2524@gmail.com>
2022-09-05 16:52:03 +0300

    add: create tables

--- backend/builder/models.py
+++ backend/builder/models.py
@@ -2,9 +2,31 @@ from django.db import models
 
 # Create your models here.
 
+class Catagories(models.Model):
+    name= models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    def __str__(self):
+        return str(self.name)
+
+
 class Pages(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=100)
+    Page_pic = models.ImageField(null=True, blank=True, upload_to = 'profiles/', default='page.png')
     html = models.TextField()
     css = models.TextField()
+    def __str__(self):
+        return str(self.name)
+
+
     
+class Templetes(models.Model):
+    catagory = models.ForeignKey(
+        Catagories, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=50)
+    Template_pic = models.ImageField(null=True, blank=True, upload_to = 'profiles/', default='template.png')
+    description = models.CharField(max_length=100)
+    html = models.TextField()
+    css = models.TextField()
+    def __str__(self):
+        return str(self.name)
